874a1a2a31479b434c83dfcb564dc16b8a25709c
Johannes Choo <jhanschoo@gmail.com>
2025-01-14 19:45:05 +0800

    init harness

---
+++ KiliLean.lean
@@ -0,0 +1,5 @@
+-- This module serves as the root of the `KiliLean` library.
+-- Import modules here that should be built as part of the library.
+import KiliLean.AIME_Number_Theory_710
+import KiliLean.AIME_Number_Theory_717
+import KiliLean.AIME_Number_Theory_716
